088217a2f6a52ff404251c97df28f65b15b699dd
Otavio Freitas <otavioj105@gmail.com>
2025-03-25 17:38:32 -0300

    first commit

---
+++ test/model/campo_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+main(){
+  group('Campo', () {
+    test('Abrir Campo COM Exmplosão', (){
+      
+    });
+    test('Abrir Campo SEM Exmplosão', (){
+
+    });
+    test('Adicionar NÃO Vizinho', (){
+
+    });
+    test('Adicionar Vizinho', (){
+
+    });
+  });
+}
