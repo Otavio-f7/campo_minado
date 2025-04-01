2b5c0f2aebd876d52e1fa92230d10507afb8cf6a
Otavio Freitas <otavioj105@gmail.com>
2025-04-01 17:40:42 -0300

    building full screen

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,50 +1,92 @@
 import 'package:campo_minado/components/campo_widget.dart';
 import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:campo_minado/components/tabuleiro_widget.dart';
 import 'package:campo_minado/models/campo.dart';
 import 'package:campo_minado/models/explosao_exception.dart';
+import 'package:campo_minado/models/tabuleiro.dart';
 import 'package:flutter/material.dart';
 
-class CampoMinadoApp extends StatelessWidget {
+class CampoMinadoApp extends StatefulWidget {
   const CampoMinadoApp({super.key});
 
+  @override
+  State<CampoMinadoApp> createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+
+  bool? _venceu;
+  Tabuleiro? _tabuleiro;
   _reiniciar(){
-    print('Reiniciar');
-  }
-  void _abrir(Campo c){
-    print('Abrir');
-  }
-  void _alterarMarcacao(Campo c){
-    print('Alterar Marcacao');
+    setState(() {
+      _venceu = null;
+      _tabuleiro!.reiniciar();
+    });
   }
 
-  
+  void _abrir(Campo c){
+    if(_venceu != null){return;}
+    setState(() {
+      try{
+        c.abrir();
+        if(_tabuleiro!.resolvido){
+          _venceu = true;
+        }
+      }on ExplosaoException {
+          _venceu = false;
+          _tabuleiro!.reverlarBombas();
+      }
+    });
+  }
+
+  void _alterarMarcacao(Campo c){
+    if(_venceu != null){return;}
+    setState(() {
+      c.alternarMarcacao();
+      if(_tabuleiro!.resolvido){
+         _venceu = true;
+      }
+    });
+  }
+
+  Tabuleiro? _getTabuleito(double largura, double altura){
+    if(_tabuleiro == null){
+      int qtdeColunas = 15;
+      double tamanhoCampo = largura / qtdeColunas;
+      int qtdeLinhas = (altura / tamanhoCampo).floor();
+      _tabuleiro = Tabuleiro(
+        linhas: qtdeLinhas, 
+        colunas: qtdeColunas, 
+        qtdeBomabas: 50
+      );
+    }
+    return _tabuleiro;
+  }
 
   @override
   Widget build(BuildContext context) {
-    Campo viz1 = Campo(linha: 1, coluna: 0);
-    viz1.minar();
-    Campo viz2 = Campo(linha: 1, coluna: 0);
-    viz2.minar();
-    Campo campo = Campo(linha: 0, coluna: 0);
-    campo.adicionarVizinho(viz1);
-    campo.adicionarVizinho(viz2);
-    try{
-      // campo.minar();
-      campo.alternarMarcacao();
-
-    } on ExplosaoException {}
+    
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: ResultadoWidget(
-          venceu: true, 
+          venceu: _venceu, 
           onReiniciar:_reiniciar,
         ),
         body: Container(
-          child: CampoWidget(
-            campo: campo, 
-            onAbrir: _abrir, 
-            onAlterarMarcacao: _alterarMarcacao
+          color: Colors.grey,
+          child: LayoutBuilder(
+            builder: (ctx, constraints){
+              return TabuleiroWidget(
+                tabuleiro: _getTabuleito(
+                  constraints.maxWidth, 
+                  constraints.maxHeight
+                )!, 
+                onAbrir: _abrir, 
+                onAlterarMarcacao: _alterarMarcacao
+              );
+            }
           ),
         ),
       ),
